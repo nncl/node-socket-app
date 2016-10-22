01f4dfd0deec38a665c460462c6a91ca4aee6388
= <nncl@live.com>
2016-10-22 15:47:48 -0200

    Add support for nicknames - fix #3

--- index.js
+++ index.js
@@ -9,24 +9,31 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function (socket) {
+    let nickname;
+    console.log('a user is connected');
 
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', (obj) => {
+        console.log('message: ' + JSON.stringify(obj));
+        io.emit('chat message', obj);
     });
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-        io.emit('disconnect', "user disconnected");
+    socket.on('nickname', (nick) => {
+        nickname = nick;
+        console.log('nickname: ' + nick);
+        io.emit('nickname', nick);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(nickname + ' has disconnected');
+        io.emit('disconnect', nickname + " disconnected");
     });
 });
 
-http.listen(3000, function(){
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
